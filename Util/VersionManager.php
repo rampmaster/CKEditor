73c945fd446cf708d7926b52e0e5c550837eb768
craigray <craig@craigrayner.com>
2018-02-27 10:25:40 +1100

    Version Manager Added
    Signed-off-by: craigray <craig@craigrayner.com>

---
+++ Util/VersionManager.php
@@ -0,0 +1,16 @@
+<?php
+namespace Hillrange\CKEditor\Util;
+
+
+class VersionManager
+{
+    const VERSION = '0.1.12';
+
+    /**
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        return VersionManager::VERSION;
+    }
+}
